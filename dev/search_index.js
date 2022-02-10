03d94a7bc8f20a4be9952a8d90ca44eaeab0525b
Documenter.jl <documenter@juliadocs.github.io>
2022-02-10 09:06:01 +0000

    build based on 2394df3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Wright.jl","page":"Wright.jl","title":"Wright.jl","text":"","category":"section"},{"location":"","page":"Wright.jl","title":"Wright.jl","text":"Fox-Wright function computing in Julia","category":"page"}]
+}
